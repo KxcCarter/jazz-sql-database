da2695e979e2923260c806a27f85da2e60856763
Kenneth Carter <kxccarter@gmail.com>
2020-07-07 19:04:05 -0500

    implemented moment.js in artist.router

--- server/routes/artist.router.js
+++ server/routes/artist.router.js
@@ -1,45 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-
-const Pool = pg.Pool; // Class
-
-// Connect Node to our database
-const pool = new Pool({
-    database: 'jazzy_ajax', // name of our database
-    host: 'localhost', // where is your database?
-    port: 5432, // this is the default port
-    max: 10, // number of connections
-    idleTimeoutMillis: 10000 // 10 seconds
-});
+const pool = require('../modules/pool');
+const moment = require('moment');
 
 router.get('/', (req, res) => {
     console.log(`In /artist GET`);
 
     let queryText = `SELECT * FROM "artists";`;
-    pool.query(queryText).then((result) => {
-        // send back our query results as an array of objects
-        res.send(result.rows); // result.rows will always be an Array
-    }).catch((error) => {
-        console.log(`Error in GET /artists ${error}`);
-        // 500 means "server error", generic but effective
-        res.sendStatus(500);
-    });
-    
+    pool
+        .query(queryText)
+        .then((result) => {
+            // send back our query results as an array of objects
+            const rows = result.rows;
+            for (let each of rows) {
+                each.year_born = moment(each.year_born).format('MMM Do YYYY');
+                console.log(each);
+            }
+
+            res.send(rows); // result.rows will always be an Array
+        })
+        .catch((error) => {
+            console.log(`Error in GET /artists ${error}`);
+            // 500 means "server error", generic but effective
+            res.sendStatus(500);
+        });
 });
 
 router.post('/', (req, res) => {
     console.log(`In /artist POST with`, req.body);
-    
+
     const artistToAdd = req.body;
     const queryText = `INSERT INTO "artists" ("artist_name", "year_born")
                        VALUES ($1, $2);`;
-    pool.query(queryText, [artistToAdd.name, artistToAdd.born])
+    pool
+        .query(queryText, [artistToAdd.name, artistToAdd.born])
         .then((responseFromDatabase) => {
             console.log(responseFromDatabase);
             // 201 means "created"
             res.sendStatus(201);
-        }).catch((error) => {
+        })
+        .catch((error) => {
             console.log(`Error in POST /artist ${error}`);
             res.sendStatus(500);
         });
